67a6980f1364dbf1c83a3cd545bb030ca6925f5a
Nguyen Van Hoang <Hoangculus2.5@gmail.com>
2025-04-17 02:00:05 +0700

    hoang

--- js.js
+++ js.js
@@ -1,3 +1,39 @@
+// 3 ảnh
+window.addEventListener('scroll', () => 
+{
+    document.querySelectorAll('.img_small').forEach(el => 
+    {
+        const rect = el.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom > 0)
+        {
+            // el.classList.add('album_hihi');
+            el.classList.remove('js_left');
+            el.classList.remove('js_right');
+        }
+        else
+        {
+            if(el.classList.contains('js_l'))
+            el.classList.add('js_left');
+            else
+            el.classList.add('js_right');
+        }
+    });
+});
+
+window.addEventListener('scroll', () => {
+    const el = document.querySelector('.img_big');
+    if (el) {
+        const rect = el.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom > 0) {
+            el.classList.remove('js_center');
+        } else {
+            el.classList.add('js_center');
+        }
+    }
+});
+
+
+
 window.addEventListener('scroll', () => 
 {
     document.querySelectorAll('.js_album').forEach(el => 
